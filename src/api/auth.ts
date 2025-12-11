6185dc75c0fd5a4dbb71c06cc8827f4e0f4e9a12
Mista-Log <oloyedeibrahimsmile@gmail.com>
2025-12-12 00:23:44 +0100

    updated auth.ts

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 // src/api/auth.ts
 import axios from "axios";
 
-const API_BASE = process.env.VITE_API_BASE_URL;
+const API_BASE = import.meta.env.VITE_API_BASE_URL; 
 
 export interface LoginPayload {
   email: string;
